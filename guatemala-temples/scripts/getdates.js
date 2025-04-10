40a8084f862a9b0ab080adebdc34b8e8fe4cfd6a
solok18 <ksolorzano862@gmail.com>
2025-04-10 16:37:54 -0700

    final project

---
+++ guatemala-temples/scripts/getdates.js
@@ -0,0 +1,16 @@
+const year = document.getElementById("currentyear");
+const modified = document.getElementById("lastModified")
+
+const today = new Date();
+
+// This line below shows current year
+year.innerHTML = `${today.getFullYear()}`;
+
+// This line below show the time the page was modified last. Updated everytime the page refreshes
+modified.innerHTML = `Last Modification: ${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "short",
+		timeStyle: "long"
+	}
+).format(today).replace(",","")}`; 
